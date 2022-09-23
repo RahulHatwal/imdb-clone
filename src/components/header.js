7b6e8f1544b1df960227f5a7bd57f2f1f3f424c6
Rahul Hatwal <rahulhatwaldoc@gmail.com>
2022-09-23 09:15:52 +0530

    Added Header

---
+++ src/components/header.js
@@ -0,0 +1,33 @@
+import "./header.css";
+
+function Header() {
+  let isLoggedIn = false;
+  let imageUrl =
+    "https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg";
+  return (
+    <div className="imdb-header">
+      <div className="logo">
+        <img id="logo-img" src={imageUrl} alt="logo-img" />
+      </div>
+      <div className="title">IMDB Clone</div>
+      <div className="buttons">
+        <input
+          type="button"
+          name="signup"
+          id="signupButton"
+          value="Sign Up"
+          className="headerButtons"
+        />
+        <input
+          type="button"
+          name="login"
+          id="loginButton"
+          value="Login"
+          className="headerButtons"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;

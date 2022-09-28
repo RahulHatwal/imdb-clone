be46d17c74ce35a372e8efec52758d4852e7a40d
Rahul Hatwal <rahulhatwaldoc@gmail.com>
2022-09-28 09:42:30 +0530

    Added component LandingInfo

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,13 +1,22 @@
 import "./Landing.css";
+import "./LandingInfo";
 // carousel images import
 import { moviesImages } from "../Images/CarouselCover";
 import React, { useState } from "react";
 
 // bootstrap icons import from react-icons
 import { BsArrowRightSquare, BsArrowLeftSquare } from "react-icons/bs";
+import { LandingInfo } from "./LandingInfo";
 
 // passing props
 export function Landing({ moviesImages }) {
+  const arr = [
+    "Reviews",
+    "Listings",
+    "All your favourite movies at one place",
+    "Welcome to IMDB",
+  ];
+
   const [current, setCurrent] = useState(0);
 
   const length = moviesImages.length;
@@ -51,9 +60,29 @@ export function Landing({ moviesImages }) {
         </div>
       </div>
 
-      <div class="favourites"></div>
+      {/* <div class="InfoCards Reviews">
+        <LandingInfo text="Reviews" />
+      </div>
 
-      <div class="welcome-title"></div>
+      <div class="InfoCards Listings">
+        <LandingInfo text="Listings" />
+      </div>
+
+      <div class="InfoCards favourites">
+        <LandingInfo text="All your favourite movies at one place" />
+      </div>
+
+      <div class="InfoCards welcome-msg">
+        <LandingInfo text="Welcome to IMDB" />
+      </div> */}
+
+      {arr.map((text) => {
+        return (
+          <div class="InfoCards">
+            <LandingInfo text={text} />
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/Landing/LandingInfo.js
@@ -0,0 +1,5 @@
+import "./LandingInfo.css";
+
+export const LandingInfo = ({ text }) => {
+  return <div className="infoBlock">{text}</div>;
+};
